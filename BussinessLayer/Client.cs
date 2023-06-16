ad9f8a551f0b9e40a218f03436082a540b1413de
Shepelka1 <ishepelev898@gmail.com>
2023-06-16 17:18:14 +0300

    Changed business layer and added main and ClientDbContext

--- BussinessLayer/Client.cs
+++ BussinessLayer/Client.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,13 @@ namespace BussinessLayer
 {
     public class Client
     {
+        [Key]
+        public int ClientId { get; set; }
+        public string PhoneNumber { get; set; }
+        public List<RoomReservation> Reservations { get; set; }
+        public List<RestaurantOrder> RestaurantOrders { get; set; }
+        public List<SpaSession> SpaSessions { get; set; }
+        public List<BarOrder> BarOrders { get; set; }
         private Client()
         {
             Reservations = new List<RoomReservation>();
@@ -21,11 +29,5 @@ namespace BussinessLayer
             PhoneNumber = phoneNumber;
         }
 
-        public int ClientId { get; set; }
-        public string PhoneNumber { get; set; }
-        public List<RoomReservation> Reservations { get; set; }
-        public List<RestaurantOrder> RestaurantOrders { get; set; }
-        public List<SpaSession> SpaSessions { get; set; }
-        public List<BarOrder> BarOrders { get; set; }
     }
 }
--- DataLayer/ResortDbContext.cs
+++ DataLayer/ResortDbContext.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using BussinessLayer;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataLayer
 {
-    public class ResortDbContext
+    public class ResortDbContext : DbContext
     {
+        public ResortDbContext(string nameOrConnectionString) : base(nameOrConnectionString)
+        {
+        }
+        public DbSet<Client> MyProperty { get; set; }
     }
 }
